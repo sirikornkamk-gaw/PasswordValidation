6694aedec6a62bc7ec40b74e3d5c9483bd478753
sirikornkamk-gaw <sirikorn.kamk@ku.th>
2026-07-16 17:24:05 +0700

    Add files via upload

--- src/PasswordValidator.java
+++ src/PasswordValidator.java
@@ -6,6 +6,28 @@ public class PasswordValidator {
 
     static boolean validate(String pw) {
         // TODO: implement ตาม spec ใน README.md (R1-R6)
-        return false;
+        if(pw == null) throw new IllegalArgumentException();
+        if(pw.length() == MIN_LEN) return true ;
+        if(pw.length() == MAX_LEN) return true ;
+
+       if (pw.length() < 8) return false ;
+        if(pw.length() > 20) return false ;
+
+        
+
+        boolean hasUpper = false ;
+        boolean hasLower = false ;
+        boolean hasdigit = false ;
+        boolean hasSpace = true ;
+        for(int i=0 ; i<pw.length() ; i++ ){
+            if (Character.isUpperCase(pw.charAt(i))) hasUpper = true ;
+            if (Character.isLowerCase(pw.charAt(i))) hasLower = true ;
+            if (Character.isDigit(pw.charAt(i))) hasdigit = true ;
+            if (Character.isSpaceChar(pw.charAt(i))) hasSpace = false ;
+            
+
+        }
+       
+        return hasUpper && hasLower && hasSpace && hasdigit ;
     }
 }
